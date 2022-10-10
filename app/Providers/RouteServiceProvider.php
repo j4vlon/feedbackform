48a6101129a51010f18d28769755642a6c767373
j4vlon <97957794+j4vlon@users.noreply.github.com>
2022-10-10 05:16:09 +0500

    Update RouteServiceProvider.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/feedbackform';
+    public const HOME = '/feedback';
 
     /**
      * The controller namespace for the application.
